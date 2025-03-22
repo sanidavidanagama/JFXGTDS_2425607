fee9da52015640fc7abde8f8263309460abbb2af
sanidavidanagama <sanidavidanagama@gmail.com>
2025-03-22 11:44:06 +0530

    Creating the project for javafx application

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module edu.iit.erp.jfxgtds_2425607 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens edu.iit.erp.jfxgtds_2425607 to javafx.fxml;
+    exports edu.iit.erp.jfxgtds_2425607;
+}
